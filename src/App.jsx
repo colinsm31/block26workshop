d6180f0625bc44395e6e06f9c92c0e0e9cb2f409
Colin Meyer <crmeyer@crimson.ua.edu>
2023-09-12 18:41:23 -0500

    finished tab 2 with table setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [contacts, setContacts] = useState(dummyContacts);
 
   return (
     <>
+      <ContactList />
     </>
   )
 }
